b44f657b3aa3068fceb4a55161189b9029b384ef
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 13:10:29 +0100

    revert hacks for the previous misuse of __cpuinit for hotplug only functions
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3562,7 +3562,7 @@ __acquires(&gcwq->lock)
 	}
 }
 
-static int __devinit workqueue_cpu_callback(struct notifier_block *nfb,
+static int workqueue_cpu_callback(struct notifier_block *nfb,
 						unsigned long action,
 						void *hcpu)
 {
@@ -3659,7 +3659,7 @@ static int __devinit workqueue_cpu_callback(struct notifier_block *nfb,
  * Workqueues should be brought up before normal priority CPU notifiers.
  * This will be registered high priority CPU notifier.
  */
-static int __devinit workqueue_cpu_up_callback(struct notifier_block *nfb,
+static int workqueue_cpu_up_callback(struct notifier_block *nfb,
 					       unsigned long action,
 					       void *hcpu)
 {
@@ -3677,7 +3677,7 @@ static int __devinit workqueue_cpu_up_callback(struct notifier_block *nfb,
  * Workqueues should be brought down after normal priority CPU notifiers.
  * This will be registered as low priority CPU notifier.
  */
-static int __devinit workqueue_cpu_down_callback(struct notifier_block *nfb,
+static int workqueue_cpu_down_callback(struct notifier_block *nfb,
 						 unsigned long action,
 						 void *hcpu)
 {
