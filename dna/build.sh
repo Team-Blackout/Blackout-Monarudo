201472954cb5d6b27b9e3d8a60f71efba65be879
Bryan Smith <zarboz@gmail.com>
2013-03-08 23:30:34 -0800

    updating autobuild scripts

--- dna/build.sh
+++ dna/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-make crpalmer_defconfig && \
+make beastmode_defconfig && \
 	make depend && \
 	make -j4 && \
 	make dna/update-this-version.zip && \
+	echo && echo "*********** COMPLETE *************" && echo
+	echo && echo "********EPIC FUCKING WIN**********" && echo
+	echo && echo "*****Please Collect update zip****" && echo
+	echo && echo "* Located at dna/update/         *" && echo
 	echo && echo "*********** COMPLETE *************" && echo
