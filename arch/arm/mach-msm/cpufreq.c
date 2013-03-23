500488b8f935850a76acaea134f3115ea814c769
Christopher R. Palmer <crpalmer@gmail.com>
2013-03-23 05:07:10 -0400

    Remove extra frequency setting

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -561,7 +561,6 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	INIT_WORK(&cpu_work->work, set_cpu_work);
 	init_completion(&cpu_work->complete);
 #endif
-	/* set safe default min and max speeds */
  
 	policy->max = 1512000;
 	policy->min = 384000;
