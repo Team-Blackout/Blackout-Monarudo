4048fe9151ffffe914911a60b6b0e4ee19275b76
Zarboz <zarboz@gmail.com>
2013-04-11 11:41:13 -0700

    Revert previous commit

--- arch/arm/mach-msm/board-monarudo-gpu.c
+++ arch/arm/mach-msm/board-monarudo-gpu.c
@@ -440,11 +440,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 550000000,
-			.bus_freq = 5,
-			.io_fraction = 0,
-		},
-		{
-			.gpu_freq = 500000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
@@ -458,6 +453,12 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 2,
 			.io_fraction = 100,
 		},
+		{
+			.gpu_freq = 128000000,
+			.bus_freq = 1,
+			.io_fraction = 100,
+		},
+
 		{
 			.gpu_freq = 27000000,
 			.bus_freq = 0,
